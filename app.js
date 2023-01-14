ae5e21298b33d9aabae5810ef34f981ae00a411a
Henrique Ribeiro <84481957+Heriq86@users.noreply.github.com>
2023-01-14 16:15:00 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strinct'
+
+const switcher =document.querySelector('.btn');
+
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme')
+
+    var classNome = document.body.className;
+    if(classNome == "light-theme") {
+    this.textContent = "Dark";
+    }
+    else {
+    this.textContent = "Light";
+    }
+    console.log('troquei o tema:' + classNome);
+
+});
